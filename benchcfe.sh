1cd102a0481b2a4670a1ee88060bd2feb19cb990
Alfonso de la Guarda <alfonsodg@gmail.com>
2023-08-30 10:22:28 -0500

    Fix bug for hostnames in files

--- benchcfe.sh
+++ benchcfe.sh
@@ -7,17 +7,17 @@ apt install sysbench -y
 apt install iperf3 -y
 echo "Starting tests...."
 HN=`hostname`
-sysbench cpu --threads=2 run | grep 'events per second:\|events (avg/stddev):' > data_cpu_test_$HN_1
+sysbench cpu --threads=2 run | grep 'events per second:\|events (avg/stddev):' > data_cpu_test_1_$HN
 sleep 5
-sysbench cpu --threads=2 run | grep 'events per second:\|events (avg/stddev):' > data_cpu_test_$HN2
+sysbench cpu --threads=2 run | grep 'events per second:\|events (avg/stddev):' > data_cpu_test_2_$HN
 sleep 5
-sysbench cpu --threads=2 run | grep 'events per second:\|events (avg/stddev):' > data_cpu_test_$HN3
+sysbench cpu --threads=2 run | grep 'events per second:\|events (avg/stddev):' > data_cpu_test_3_$HN
 sleep 5
-fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --filename=test --bs=4k --iodepth=64 --size=4G --readwrite=randrw --rwmixread=75 | grep 'read:\|write:' | awk {'print ($1$2) ($4)'} | sed 's/;//g' | sed "s/([^)]*msec)/ /g" > data_disk_test_$HN_1
+fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --filename=test --bs=4k --iodepth=64 --size=4G --readwrite=randrw --rwmixread=75 | grep 'read:\|write:' | awk {'print ($1$2) ($4)'} | sed 's/;//g' | sed "s/([^)]*msec)/ /g" > data_disk_test_1_$HN
 sleep 5
-fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --filename=test --bs=4k --iodepth=64 --size=8G --readwrite=randrw --rwmixread=75 | grep 'read:\|write:' | awk {'print ($1$2) ($4)'} | sed 's/;//g' | sed "s/([^)]*msec)/ /g" > data_disk_test_$HN_2
+fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --filename=test --bs=4k --iodepth=64 --size=8G --readwrite=randrw --rwmixread=75 | grep 'read:\|write:' | awk {'print ($1$2) ($4)'} | sed 's/;//g' | sed "s/([^)]*msec)/ /g" > data_disk_test_2_$HN
 sleep 5
-fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --filename=test --bs=4k --iodepth=64 --size=16G --readwrite=randrw --rwmixread=75 | grep 'read:\|write:' | awk {'print ($1$2) ($4)'} | sed 's/;//g' | sed "s/([^)]*msec)/ /g" > data_disk_test_$HN_3
+fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --filename=test --bs=4k --iodepth=64 --size=16G --readwrite=randrw --rwmixread=75 | grep 'read:\|write:' | awk {'print ($1$2) ($4)'} | sed 's/;//g' | sed "s/([^)]*msec)/ /g" > data_disk_test_3_$HN
 sleep 5
 nohup ./primes.sh &
 sysbench cpu --threads=2 run | grep 'events per second:\|events (avg/stddev):' > data_mixed_cpu_result_$HN
