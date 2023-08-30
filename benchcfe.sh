2e89ee9e31c3cfc5345867a90e87004ff88ea1d3
Alfonso de la Guarda <alfonsodg@gmail.com>
2023-08-30 10:09:04 -0500

    Improved project and documentation

--- benchcfe.sh
+++ benchcfe.sh
@@ -5,18 +5,25 @@ apt upgrade -y
 apt install fio -y
 apt install sysbench -y
 apt install iperf3 -y
-sysbench cpu --threads=2 run | grep 'events per second:\|events (avg/stddev):' > cpu_test_1
+echo "Starting tests...."
+HN=`hostname`
+sysbench cpu --threads=2 run | grep 'events per second:\|events (avg/stddev):' > data_cpu_test_$HN_1
 sleep 5
-sysbench cpu --threads=2 run | grep 'events per second:\|events (avg/stddev):' > cpu_test_2
+sysbench cpu --threads=2 run | grep 'events per second:\|events (avg/stddev):' > data_cpu_test_$HN2
 sleep 5
-sysbench cpu --threads=2 run | grep 'events per second:\|events (avg/stddev):' > cpu_test_3
+sysbench cpu --threads=2 run | grep 'events per second:\|events (avg/stddev):' > data_cpu_test_$HN3
 sleep 5
-fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --filename=test --bs=4k --iodepth=64 --size=4G --readwrite=randrw --rwmixread=75 | grep 'read:\|write:' | awk {'print ($1$2) ($4)'} | sed 's/;//g' | sed "s/([^)]*msec)/ /g" > disk_test_1
+fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --filename=test --bs=4k --iodepth=64 --size=4G --readwrite=randrw --rwmixread=75 | grep 'read:\|write:' | awk {'print ($1$2) ($4)'} | sed 's/;//g' | sed "s/([^)]*msec)/ /g" > data_disk_test_$HN_1
 sleep 5
-fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --filename=test --bs=4k --iodepth=64 --size=8G --readwrite=randrw --rwmixread=75 | grep 'read:\|write:' | awk {'print ($1$2) ($4)'} | sed 's/;//g' | sed "s/([^)]*msec)/ /g" > disk_test_2
+fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --filename=test --bs=4k --iodepth=64 --size=8G --readwrite=randrw --rwmixread=75 | grep 'read:\|write:' | awk {'print ($1$2) ($4)'} | sed 's/;//g' | sed "s/([^)]*msec)/ /g" > data_disk_test_$HN_2
 sleep 5
-fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --filename=test --bs=4k --iodepth=64 --size=16G --readwrite=randrw --rwmixread=75 | grep 'read:\|write:' | awk {'print ($1$2) ($4)'} | sed 's/;//g' | sed "s/([^)]*msec)/ /g" > disk_test_3
+fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --filename=test --bs=4k --iodepth=64 --size=16G --readwrite=randrw --rwmixread=75 | grep 'read:\|write:' | awk {'print ($1$2) ($4)'} | sed 's/;//g' | sed "s/([^)]*msec)/ /g" > data_disk_test_$HN_3
 sleep 5
 nohup ./primes.sh &
-sysbench cpu --threads=2 run | grep 'events per second:\|events (avg/stddev):' > mixed_cpu_result
-fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --filename=test --bs=4k --iodepth=64 --size=4G --readwrite=randrw --rwmixread=75 | grep 'read:\|write:' | awk {'print ($1$2) ($4)'} | sed 's/;//g' | sed "s/([^)]*msec)/ /g" > mixed_disk_result
+sysbench cpu --threads=2 run | grep 'events per second:\|events (avg/stddev):' > data_mixed_cpu_result_$HN
+fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --filename=test --bs=4k --iodepth=64 --size=4G --readwrite=randrw --rwmixread=75 | grep 'read:\|write:' | awk {'print ($1$2) ($4)'} | sed 's/;//g' | sed "s/([^)]*msec)/ /g" > data_mixed_disk_result_$HN
+cat /proc/cpuinfo > data_cpu_info_$HN
+cat /proc/meminfo > data_mem_info_$HN
+cat df -h > data_disk_info_$HN
+echo "Tests ended..."
+echo "Please colllect the data* files to fill up the spreadsheet"
